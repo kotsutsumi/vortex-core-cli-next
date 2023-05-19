f7ff0d426421bf9696829b4ac5b0eb76080a8517
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2023-05-20 04:16:31 +0900

    fixed Plain Page generator

--- src/actions/AddPage/Plain.ts
+++ src/actions/AddPage/Plain.ts
@@ -5,7 +5,7 @@ import { deployFiles } from '../../commands'
 
 export default async function (opts: any) {
     // deploy files
-    await deployFiles(opts.src, `${opts.dest}/app/${opts.page_path}`, {
+    await deployFiles(opts.src, `${opts.dest}`, {
         page_path: opts.page_path
     })
 
