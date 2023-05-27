2c804545812549acdd84397d1d59d3fa34a01a2e
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2023-05-27 15:12:53 +0900

    fixed document object could not error

--- templates/Dashboard/libs/vortexcore/misc/isDarkMode.ts
+++ templates/Dashboard/libs/vortexcore/misc/isDarkMode.ts
@@ -3,6 +3,10 @@
 'use client'
 
 export default () => {
+    if (typeof document === 'undefined') {
+        return false
+    }
+
     return document.body.classList.contains('dark-mode')
 
     //
